02fe8d1eae649c3145d07e44c1e94ea62cc5f908
a.fialko <a.fialko@corp.mail.ru>
2021-03-15 15:35:46 +0300

    buildable

--- app/src/main/java/ru/park/Constants.java
+++ app/src/main/java/ru/park/Constants.java
@@ -1,8 +1,5 @@
 package ru.park;
 
 class Constants {
-    private static class InternalConstants {
-        public static final String EXTRA_INT = "extra_int";
-    }
-
+    public static final String EXTRA_INT = "extra_int";
 }
--- app/src/main/java/ru/park/MainActivity.java
+++ app/src/main/java/ru/park/MainActivity.java
@@ -14,7 +14,6 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
     int counter = 0;
 
     @Override
